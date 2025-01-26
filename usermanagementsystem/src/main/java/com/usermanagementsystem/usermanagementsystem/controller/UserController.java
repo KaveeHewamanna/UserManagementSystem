d8352ecfef3411bbea3c37476fb3893918bd38c8
Kavee Hewamanna <hewamannakavee@gmail.com>
2025-01-26 09:40:51 +0530

    Exception handling.

--- usermanagementsystem/src/main/java/com/usermanagementsystem/usermanagementsystem/controller/UserController.java
+++ usermanagementsystem/src/main/java/com/usermanagementsystem/usermanagementsystem/controller/UserController.java
@@ -1,26 +1,47 @@
 package com.usermanagementsystem.usermanagementsystem.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.usermanagementsystem.usermanagementsystem.exception.UserNotFoundException;
 import com.usermanagementsystem.usermanagementsystem.model.User;
 import com.usermanagementsystem.usermanagementsystem.repository.UserRepository;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000")
+
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-    
+
 // admin=user (path)
     @PostMapping("/admin")
     User newUser(@RequestBody User newUser) {
         
         return userRepository.save(newUser);
     }
-    
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+    // Get user with specific id
+    @GetMapping("/admin/{id}")
+    User getUserBId(@PathVariable Long id){
+        return userRepository.findById(id)
+        .orElseThrow(()->new UserNotFoundException(id));
+    }
 
     
 
